d61eb036ce2369d1c7fb2f1eadd2eb668478b4e7
ldu13 <90493091+ldu13@users.noreply.github.com>
2022-10-16 10:44:18 -0400

    Update LibAppMain.cpp

--- MS51/LibAppMain.cpp
+++ MS51/LibAppMain.cpp
@@ -1,13 +1,3 @@
-// Final Project
-// Seneca Library Application Main Module
-// File LibAppMain.cpp
-// Version 1.0
-// Author	Fardad Soleimanloo
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-//                    2021-11-27           Initial release
-/////////////////////////////////////////////////////////////////
 #include "LibApp.h"
 #include "Date.h"
 
