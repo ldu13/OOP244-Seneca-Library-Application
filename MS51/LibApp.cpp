a0a05923c01d036a69eec7bd0438fa4358de33f6
ldu13 <90493091+ldu13@users.noreply.github.com>
2022-10-16 10:43:19 -0400

    Update LibApp.cpp

--- MS51/LibApp.cpp
+++ MS51/LibApp.cpp
@@ -1,15 +1,3 @@
-/*
-	==================================================
-	Final Project (Milestone 5):
-	==================================================
-	Name   :           Lei Du
-	Seneca email:      ldu13@myseneca.ca
-	Seneca Student ID: 047587134
-	Date:              December 7, 2021
-	Section:           NCC
-*/
-// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments. My professor helped me modify the intCheck() function in Menu.cpp.
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <iomanip>
 #include <iostream>
